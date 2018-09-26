bafdb79a5846b988a423e0bca9202e47fd38b889
acbaumill <43015458+acbaumill@users.noreply.github.com>
2018-09-26 16:36:50 -0400

    Add files via upload
     Just prototype/thinking it out for main(), supervisor() and some connections. There will be a second set of StoreDataAccess/StoreFacade relabeled as EmployeeDataAccess/EmployeeFacade for the secondary database (Terry)

---
+++ MainStore.java
@@ -0,0 +1,14 @@
+/** CSCI 490-01
+ * 
+ * @author Adam Baumiller
+ * MainStore is the command
+ *
+ */
+public class MainStore {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
