e3d8bfa432db7ec2d248d5eca600a3651d3ca3ef
csterryCoastal <43015720+csterryCoastal@users.noreply.github.com>
2018-10-03 16:55:14 -0400

    Add files via upload

---
+++ Manager.java
@@ -0,0 +1,54 @@
+/**
+ * 
+ */
+
+/**
+ * @author Terry
+ *
+ */
+public class Manager {
+	
+	private int authorityLevel=3;
+	private String UserName;
+	private int PIN;
+	
+	public Manager() {
+		
+	}
+	public static void addEmployee(String firstName, String lastName, String PIN, String position, String status)
+	{
+		
+	}
+	
+	public static void removeEmployee(String firstName, String lastName, String PIN, String position, String status)
+	{
+		
+	}
+	
+	public static void addSupervisor(String userName, int PIN, int authorityLevel) 
+	{
+		
+	}
+	
+	public static void removeSupervisor(String userName, int PIN, int authorityLevel) 
+	{
+		
+	}
+	
+	//Method to BRUTE FORCE program. Have no limits, can do anything from this.
+	public static void BRUTE(int authorityLevel) {
+		authorityLevel = 3;
+		
+	}
+
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
