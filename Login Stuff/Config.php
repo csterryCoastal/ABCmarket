7af71c93441a0bc36b66858775719d8546946fb6
csterryCoastal <43015720+csterryCoastal@users.noreply.github.com>
2018-10-22 16:34:09 -0400

    Update Config.php

--- Login Stuff/Config.php
+++ Login Stuff/Config.php
@@ -1,7 +1,7 @@
 <?php
-   define('DB_SERVER', 'localhost:3036');
+   define('DB_SERVER', 'localhost:3306');
    define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'rootpassword');
-   define('DB_DATABASE', 'database');
+   define('DB_PASSWORD', 'csci330pass');
+   define('DB_DATABASE', 'employeeDB');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
